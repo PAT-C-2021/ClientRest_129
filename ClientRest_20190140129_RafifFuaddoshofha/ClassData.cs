7237470c1823e0a9946a841dd4d49f5335df9abb
rafiffuaddo <rafiffuad3108@gmail.com>
2022-01-04 01:07:34 +0700

    Add project files.

---
+++ ClientRest_20190140129_RafifFuaddoshofha/ClassData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_20190140129_RafifFuaddoshofha.Program;
+
+namespace ClientRest_20190140129_RafifFuaddoshofha
+{
+    internal class ClassData
+    {
+        internal void getData()
+        {
+            //var type data yang tidak peduli pada bentuk, tapi isinya
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("NIM : " + mhs.nim);
+                Console.WriteLine("Nama : " + mhs.nama);
+                Console.WriteLine("Prodi : " + mhs.prodi);
+                Console.WriteLine("Angkatan : " + mhs.angkatan);
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
